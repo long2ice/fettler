464876f6ea0f0d058b90720d9f65ab032d5edf9b
long2ice <long2ice@gmail.com>
2021-03-03 22:19:43 +0800

    update

--- mycacher/cli.py
+++ mycacher/cli.py
@@ -1,21 +1,25 @@
 import typer
+from dynaconf import settings
 
 app = typer.Typer()
 
-
-@app.command()
-def consumer():
-    pass
+config_option = typer.Option('./config.yml', "-c", "--config", help='Config file.')
 
 
 @app.command()
-def producer():
-    pass
+def consumer(config: str = config_option):
+    settings.load_file(config)
 
 
 @app.command()
-def server():
-    pass
+def producer(config: str = config_option):
+    settings.load_file(config)
+
+
+@app.command()
+def server(config: str = config_option):
+    settings.load_file(config, env='')
+    print(settings.items())
 
 
 if __name__ == "__main__":
